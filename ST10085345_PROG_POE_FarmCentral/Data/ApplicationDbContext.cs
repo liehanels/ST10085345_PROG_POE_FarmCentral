8d53fedd3e82953b144c626727af2109aae1fbdf
Liehan Els <liehan13@gmail.com>
2023-05-29 22:37:33 +0200

    Add project files.

---
+++ ST10085345_PROG_POE_FarmCentral/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ST10085345_PROG_POE_FarmCentral.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
